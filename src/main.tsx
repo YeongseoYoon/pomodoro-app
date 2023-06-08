a2375ff86af6d099bc955ef005b1ee7529a60a3e
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-06-08 13:43:07 +0900

    :lipstick: 글로벌 스타일 적용
    글로벌 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./styles/reset.css";
+import "./styles/global.css";
+
 import router from "./Router";
 import { RouterProvider } from "react-router-dom";
 
